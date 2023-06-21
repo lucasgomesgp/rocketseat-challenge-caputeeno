e53f8e71521042aa7745b64651482d9a56986942
Lucas Gomes <lucasgomesbt@gmail.com>
2023-06-21 16:49:54 -0300

    feat: refresh page when user choose on category

--- frontend/src/components/ProductsMenu.tsx
+++ frontend/src/components/ProductsMenu.tsx
@@ -7,20 +7,42 @@ import {
   Option,
 } from "@/styles/ProductsMenu";
 import Pagination from "./Pagination";
+import { useFilter } from "@/hooks/useFilter";
 
 export default function ProductsMenu() {
+  const {setFilterQuery} = useFilter();
+  async function handleFilterProduct(filter?: string) {
+    const query = filter ? `(filter:{category:"${filter}"})` : "";
+    setFilterQuery(query);
+  }
   return (
     <ContainerMenu>
       <Options>
-        <ProductName>Todos os produtos</ProductName>
-        <ProductName>Camisetas</ProductName>
-        <ProductName>Canecas</ProductName>
+        <ProductName
+          onClick={() => {
+            handleFilterProduct();
+          }}
+        >
+          Todos os produtos
+        </ProductName>
+        <ProductName
+          onClick={() => {
+            handleFilterProduct("t-shirts");
+          }}
+        >
+          Camisetas
+        </ProductName>
+        <ProductName
+          onClick={() => {
+            handleFilterProduct("mugs");
+          }}
+        >
+          Canecas
+        </ProductName>
       </Options>
       <ContainerFilters>
         <FilterBy>
-          <Option value="none">
-            Organizar por
-          </Option>
+          <Option value="none">Organizar por</Option>
           <Option value="new">Novidades</Option>
           <Option value="highToLowPrice">Preço: Maior - menor</Option>
           <Option value="lowToHighPrice">Preço: Menor - maior</Option>
