60b840e88c3b3e772f4c362795c3ba38a6483bce
Lucas Gomes <lucasgomesbt@gmail.com>
2023-06-26 14:47:56 -0300

    fix: add functions and state on hooks

--- frontend/src/hooks/useProduct.tsx
+++ frontend/src/hooks/useProduct.tsx
@@ -3,5 +3,21 @@ import { useContext } from "react";
 
 export function useProduct() {
   const { items, setItems } = useContext(ProductContext);
-  return { items, setItems };
+
+  function getSubTotalPrice() {
+    let price = 0;
+    items.map((item) => {
+      const currentItemPrice = item.price_in_cents / 100;
+      price += currentItemPrice * item.quantity;
+    });
+    return price;
+  }
+  function getQuantityOfProducts() {
+    let quantity = 0;
+    items.map((item) => {
+      quantity += item.quantity;
+    });
+    return quantity;
+  }
+  return { items, setItems, getSubTotalPrice, getQuantityOfProducts };
 }
--- frontend/src/hooks/useStorage.tsx
+++ frontend/src/hooks/useStorage.tsx
@@ -1,24 +1,63 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Product } from "@/types/Home";
 import { useEffect, useState } from "react";
 import { useProduct } from "./useProduct";
+import { ProductItem } from "@/contexts/Product";
+import { removeItem, searchIfElementHasAdded } from "@/utils/check-element";
 
-export function useStorage(name: string) {
-  const {items, setItems} = useProduct();
+export function useStorage() {
+  const { items, setItems } = useProduct();
+  const [itemsStoraged, setItemsStoraged] = useState<ProductItem[]>([]);
 
-  function setItemOnStorage(item: Product){
-    setItems([...items, item]);
-    localStorage.removeItem("products");
-    localStorage.setItem("products", JSON.stringify(items));
+  function setItemOnStorage(item: ProductItem) {
+    const itemsFiltered = searchIfElementHasAdded(item, items);
+    if (items.length === 0) {
+      setItemsStoraged([item]);
+      localStorage.setItem("products", JSON.stringify([item]));
+    } else if (itemsFiltered.length >= 1) {
+      let newItems = items.map((currentItem) => {
+        const isElementChoosed = currentItem.id === item.id;
+        const element = isElementChoosed
+          ? {
+              ...currentItem,
+              quantity: currentItem.quantity + 1,
+            }
+          : { ...currentItem };
+        return element;
+      });
+      setItemsStoraged(newItems);
+      localStorage.setItem("products", JSON.stringify(newItems));
+    } else {
+      setItemsStoraged([...items, item]);
+      localStorage.setItem("products", JSON.stringify([...items, item]));
+    }
+  }
+
+  function changeQuantity(item: ProductItem) {
+    let itemsUpdated = items.map((currentItem) => {
+      const isElementChoosed = currentItem.id === item.id;
+      const element = isElementChoosed
+        ? {
+            ...item,
+          }
+        : { ...currentItem };
+      return element;
+    });
+    setItemsStoraged(itemsUpdated);
+    localStorage.setItem("products", JSON.stringify(itemsUpdated));
+  }
+  function removeItemOnStorage(itemId: string) {
+    const newItems = removeItem(itemId, items);
+    setItemsStoraged(newItems);
+    localStorage.setItem("products", JSON.stringify(newItems));
   }
   useEffect(() => {
     if (typeof window !== undefined) {
-      const itemsSaved = localStorage.getItem(name);
-      if(itemsSaved){
+      const itemsSaved = localStorage.getItem("products");
+      if (itemsSaved) {
         const itemsParsed = JSON.parse(itemsSaved);
         setItems(itemsParsed);
       }
     }
-  }, []);
-  return {items, setItemOnStorage };
+  }, [itemsStoraged]);
+  return { items, setItemOnStorage, removeItemOnStorage, changeQuantity };
 }
